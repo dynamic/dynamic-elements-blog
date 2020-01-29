a0df09aa65249c03e351c29ab75e1f7400156e48
cpenny <cpenny@silverstripe.com>
2020-01-29 15:10:26 +1300

    Add new Overview/Pagination/Widgets blocks

---
+++ tests/Elements/ElementBlogPaginationTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dynamic\Elements\Blog\Elements\Tests;
+
+use Dynamic\Elements\Blog\Elements\ElementBlogPagination;
+use SilverStripe\Dev\SapphireTest;
+
+/**
+ * Class ElementBlogPaginationTest
+ *
+ * @package Dynamic\Elements\Blog\Elements\Tests
+ */
+class ElementBlogPaginationTest extends SapphireTest
+{
+    /**
+     * @var bool
+     */
+    protected $usesDatabase = true;
+
+    public function testPopulateDefaults(): void {
+        $block = ElementBlogPagination::create();
+
+        $this->assertFalse((bool) $block->ShowTitle);
+        $this->assertTrue((bool) $block->ShowPagination);
+        $this->assertFalse((bool) $block->ShowWidgets);
+    }
+}
---
+++ tests/Elements/ElementBlogWidgetsTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Dynamic\Elements\Blog\Elements\Tests;
+
+use Dynamic\Elements\Blog\Elements\ElementBlogWidgets;
+use SilverStripe\Dev\SapphireTest;
+
+/**
+ * Class ElementBlogPaginationTest
+ *
+ * @package Dynamic\Elements\Blog\Elements\Tests
+ */
+class ElementBlogWidgetsTest extends SapphireTest
+{
+    /**
+     * @var bool
+     */
+    protected $usesDatabase = true;
+
+    public function testPopulateDefaults(): void {
+        $block = ElementBlogWidgets::create();
+
+        $this->assertFalse((bool) $block->ShowTitle);
+        $this->assertFalse((bool) $block->ShowPagination);
+        $this->assertTrue((bool) $block->ShowWidgets);
+    }
+}
